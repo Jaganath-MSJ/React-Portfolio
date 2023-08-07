52e77933511d17de09abd85b28f8350f24816e1a
Jaganath M S <jagan.msjc@gmail.com>
2023-08-07 09:08:38 +0530

    adding link to connect me in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,12 @@ import React from "react";
 import "./style/Home.css";
 import { Data } from "../Data/data";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faTelegram } from "@fortawesome/free-brands-svg-icons";
+
 function Home() {
   return (
     <section className="home" id="Home">
@@ -9,6 +15,20 @@ function Home() {
         <h3>Hey! I am</h3>
         <h1>{Data.Home.name}</h1>
         <h2>I am {Data.Home.role}.</h2>
+        <div className="connectMe">
+            <a href={Data.Footer.linkedIn} target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a href={Data.Footer.github} target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a href={"mailto:" + Data.Footer.mail} target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </a>
+            <a href={Data.Footer.telegram} target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faTelegram} />
+            </a>
+          </div>
       </div>
     </section>
   );
